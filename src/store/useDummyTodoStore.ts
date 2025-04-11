79a4185409fdb6906aa225a0a86355f7aba7d29b
gyubeom <qja0707@gmail.com>
2025-04-11 12:39:41 +0900

    feat: translate dummy todo items from Korean to English

--- src/store/useDummyTodoStore.ts
+++ src/store/useDummyTodoStore.ts
@@ -12,16 +12,16 @@ interface TodoDummyList {
 
 const useDummyTodoStore = create<TodoDummyList>(set => ({
   todo: [
-    {id: '1', text: '고양이 밥 주기'},
-    {id: '2', text: '정원에 물 주기'},
-    {id: '3', text: '책 한 권 읽기'},
-    {id: '4', text: '커피 한 잔 즐기기'},
-    {id: '5', text: '회의 준비하기'},
-    {id: '6', text: '이메일 확인하기'},
-    {id: '7', text: '집 청소하기'},
-    {id: '8', text: '산책하기'},
-    {id: '9', text: '식료품 쇼핑하기'},
-    {id: '10', text: '저녁 요리하기'},
+    {id: '1', text: 'Feed the cat'},
+    {id: '2', text: 'Water the garden'},
+    {id: '3', text: 'Read a book'},
+    {id: '4', text: 'Enjoy a cup of coffee'},
+    {id: '5', text: 'Prepare for meeting'},
+    {id: '6', text: 'Check emails'},
+    {id: '7', text: 'Clean the house'},
+    {id: '8', text: 'Go for a walk'},
+    {id: '9', text: 'Grocery shopping'},
+    {id: '10', text: 'Cook dinner'},
   ],
   setMoveableTodo: todoList => set({todo: todoList}),
 }));
